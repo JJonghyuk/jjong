4255bddea202f7cbf1e84a8a5cb3d8058fd72b83
JJonghyuk <kilerjong@naver.com>
2025-08-25 10:09:10 +0900

    [2025.08.25] - next.js

--- react-next_js/app/(home)/page.tsx
+++ react-next_js/app/(home)/page.tsx
@@ -2,10 +2,18 @@ export const metadata = {
   title: "Home",
 };
 
-export default function Page() {
-  return (
-    <div>
-      <h1>Welcome to My Page</h1>
-    </div>
-  );
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+  // return fetch(URL).then(response => response.json());
+  // 한줄로 표현 가능하다.
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function Page() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
 }
